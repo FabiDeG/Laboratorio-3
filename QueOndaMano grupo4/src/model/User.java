fde52af380ab247bf277454d0fd09795431acb33
FabiDeG <fabiola.d.g.12@gmail.com>
2022-10-12 12:58:42 -0600

    User start

--- QueOndaMano grupo4/src/model/User.java
+++ QueOndaMano grupo4/src/model/User.java
@@ -4,7 +4,6 @@
 package model;
 
 /**
- * @author fabio
  *
  */
 public class User {
@@ -14,6 +13,29 @@ public class User {
 	 */
 	public User() {
 		// TODO Auto-generated constructor stub
+		
 	}
 
+	/**
+	 * Se declaran las varibles
+	 */
+	
+	protected int tipo;
+	public static final int NUser = 0;
+	public static final int NAdmin = 1;
+	
+	/**
+	 * get tipo
+	 */
+	
+	public int getTipo() {
+		return tipo;
+	}
+
+	/**
+	 * set tipo
+	 */
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
 }
