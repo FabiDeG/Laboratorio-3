77e424ed525e105c40776da1d8031a55997438f9
chuy-zip <74995966+chuy-zip@users.noreply.github.com>
2022-10-12 14:53:26 -0600

    Implementacion de la clase TxtPost (Posts de texto)

---
+++ QueOndaMano grupo4/src/model/TxtPost.java
@@ -0,0 +1,31 @@
+package model;
+
+public class TxtPost extends Post{
+	
+	private String PostMSG;
+	
+	/**
+	 * Este es el segundo tipo de post posible, un post que es un mensaje de texto
+	 * El tipo de post es 2
+	 * @param Author
+	 * @param Date
+	 * @param txtForPost, es el texto que el usuarios desea poner en su post de texto
+	 */
+	public TxtPost(String Author, String Date, String txtForPost){
+		super(Author, Date);
+		setPostType(2);
+		setPostMSG(txtForPost);
+		
+	}
+
+	public String getPostMSG() {
+		return PostMSG;
+	}
+
+	public void setPostMSG(String postMSG) {
+		PostMSG = postMSG;
+	}
+		
+	
+
+}
