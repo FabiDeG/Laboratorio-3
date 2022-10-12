2b6e4d3a29f973c1c123e9bbe8bb6c0f58f48c1b
FabiDeG <fabiola.d.g.12@gmail.com>
2022-10-12 14:13:11 -0600

    Update InitialWindow.java

--- QueOndaMano grupo4/src/UI/InitialWindow.java
+++ QueOndaMano grupo4/src/UI/InitialWindow.java
@@ -6,6 +6,9 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
 
 public class InitialWindow extends JFrame {
 
@@ -32,11 +35,35 @@ public class InitialWindow extends JFrame {
 	 */
 	public InitialWindow() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 465, 362);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("¡Bienvenid@ a QuéOndaMano!");
+		lblNewLabel.setFont(new Font("Monospaced", Font.PLAIN, 17));
+		lblNewLabel.setBounds(101, 23, 289, 46);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("¿Ya tienes cuenta?");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel_1.setBounds(162, 88, 143, 24);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Crea tu cuenta :)");
+		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel_1_1.setBounds(172, 178, 112, 24);
+		contentPane.add(lblNewLabel_1_1);
+		
+		JButton btnNewButton = new JButton("Log in");
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnNewButton.setBounds(172, 122, 89, 30);
+		contentPane.add(btnNewButton);
+		
+		JButton btnSignIn = new JButton("Sign in");
+		btnSignIn.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnSignIn.setBounds(172, 223, 89, 30);
+		contentPane.add(btnSignIn);
 	}
-
 }
