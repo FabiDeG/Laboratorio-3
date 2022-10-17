76ea3492ecd46db2b201ed413c297ae725f40845
chuy-zip <74995966+chuy-zip@users.noreply.github.com>
2022-10-17 13:26:10 -0600

    Updates data.csv with some test values

--- QueOndaMano grupo4/src/controller/test.java
+++ QueOndaMano grupo4/src/controller/test.java
@@ -1,5 +1,13 @@
 package controller;
+import controller.FileManager;
+import controller.AppManagement;
 
 public class test {
-
+	public static void main(String[] arg) {
+		
+		FileManager Filemanager = new FileManager();
+		
+		Filemanager.ReadFileForPassword("ramon", "234");
+		
+	}
 }
