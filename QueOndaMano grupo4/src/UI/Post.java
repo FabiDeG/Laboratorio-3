8ba3f0e947fab5617a89d479515488a74701aeff
FabiDeG <fabiola.d.g.12@gmail.com>
2022-10-18 23:12:16 -0600

    Revert "Modifications on UI"
    This reverts commit f62e7e736e37ea6583cd0c844ec505ced140dc89.

--- QueOndaMano grupo4/src/UI/Post.java
+++ QueOndaMano grupo4/src/UI/Post.java
@@ -1,100 +1,42 @@
 package UI;
-import java.awt.*;
-import java.awt.event.*;
 
-import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
-public class Post implements Runnable
-{
-  private String[] messages = new String[] {
-      "Hello?",
-      "Hey, what's up?",
-      "Where are you?",
-      "Right behind you.",
-      "Stop following me!",
-      "But you owe me money.",
-      "I'll gladly repay you on Tuesday.",
-      "You said that last week!",
-      "But now I actually have a job."
-  };
-  private int msgCounter = 0;
-  private JPanel panel;
-  private JScrollPane scrollPane;
-  private Timer timer;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
-  public static void main(String[] args)
-  {
-    SwingUtilities.invokeLater(new Post());
-  }
+public class Post extends JFrame {
 
-  public void run()
-  {
-    panel = new JPanel();
-    panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+	private JPanel contentPane;
 
-    scrollPane = new JScrollPane(panel);
-    scrollPane.setVerticalScrollBarPolicy(
-        JScrollPane.VERTICAL_SCROLLBAR_NEVER);
-    scrollPane.setAutoscrolls(true);
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Post frame = new Post();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
-    JFrame frame = new JFrame("Message App");
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
-    frame.setSize(260, 180);
-    frame.setLocationRelativeTo(null);
-    frame.setVisible(true);
+	/**
+	 * Create the frame.
+	 */
+	public Post() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
 
-    timer = new Timer(1500, new ActionListener()
-    {
-      public void actionPerformed(ActionEvent event)
-      {
-        if (msgCounter < messages.length)
-        {
-          addMessage(messages[msgCounter]);
-        }
-        else
-        {
-          timer.stop();
-        }
-      }
-    });
-    timer.start();
-  }
-
-  private void addMessage(String text)
-  {
-    boolean rightAligned = msgCounter % 2 != 0;
-    Color color = rightAligned ? Color.CYAN : Color.ORANGE;
-
-    JLabel label = new JLabel(text);
-    label.setOpaque(true);
-    label.setBackground(color);
-    label.setBorder(BorderFactory.createCompoundBorder(
-        BorderFactory.createLineBorder(Color.BLUE),
-        BorderFactory.createEmptyBorder(2,4,2,4)
-    ));
-
-    JPanel p = new JPanel();
-    p.setLayout(new BoxLayout(p, BoxLayout.X_AXIS));
-    p.setBorder(BorderFactory.createEmptyBorder(2,4,2,4));
-
-    if (rightAligned)
-    {
-      p.add(Box.createHorizontalGlue());
-      p.add(label);
-    }
-    else
-    {
-      p.add(label);
-      p.add(Box.createHorizontalGlue());
-    }
-
-    panel.add(p);
-    panel.revalidate();
-    int x = panel.getPreferredSize().width;
-    int y = panel.getPreferredSize().height;
-    panel.scrollRectToVisible(new Rectangle(x-1 ,y-1, 1, 1));
-
-    msgCounter++;
-  }
 }
--- QueOndaMano grupo4/src/UI/content.java
+++ QueOndaMano grupo4/src/UI/content.java
@@ -6,9 +6,6 @@ import javax.swing.JInternalFrame;
 import javax.swing.JScrollBar;
 import javax.swing.JTextField;
 import javax.swing.JList;
-import java.awt.ScrollPane;
-import java.awt.Panel;
-import javax.swing.JLabel;
 
 /**
  * Pantalla extra dentro de Jframe que se encargara de mostrar los post de las personas
@@ -38,22 +35,6 @@ public class content extends JInternalFrame {
 	 */
 	public content() {
 		setBounds(100, 100, 1037, 575);
-		getContentPane().setLayout(null);
-		
-		
-		Panel panel = new Panel();
-		panel.setBounds(25, 16, 503, 100);
-		getContentPane().add(panel);
-		panel.setLayout(null);
-		
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setBounds(14, 11, 66, 13);
-		panel.add(lblNewLabel);
-		
-		JLabel lblNewLabel_1 = new JLabel("New label");
-		lblNewLabel_1.setBounds(13, 38, 154, 27);
-		panel.add(lblNewLabel_1);
 
-		
 	}
 }
