a7ca99acb8da7d33541b931c9bf8abcb9c1e416c
chuy-zip <74995966+chuy-zip@users.noreply.github.com>
2022-10-20 14:26:05 -0600

    Added UI elements to UploadMedia Window

--- QueOndaMano grupo4/src/UI/UploadMediaWindow.java
+++ QueOndaMano grupo4/src/UI/UploadMediaWindow.java
@@ -2,10 +2,24 @@ package UI;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import controller.AppManagement;
+import controller.FileManager;
+import model.ImagePost;
+import model.Video;
+
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
+import javax.swing.JLabel;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
 
 /**
  * Pantalla en la que el usuario puede subir sus archivos para luego publicarlos
@@ -15,6 +29,8 @@ import javax.swing.border.EmptyBorder;
 public class UploadMediaWindow extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textFieldLink;
+	
 
 	/**
 	 * Launch the application.
@@ -23,7 +39,7 @@ public class UploadMediaWindow extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					UploadMediaWindow frame = new UploadMediaWindow();
+					UploadMediaWindow frame = new UploadMediaWindow(null);
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -35,13 +51,87 @@ public class UploadMediaWindow extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public UploadMediaWindow() {
+	public UploadMediaWindow(MainWindow mainframe) {
+		
+		FileManager PostManager = new FileManager();
+		AppManagement AppManage = new AppManagement();
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textFieldLink = new JTextField();
+		textFieldLink.setBounds(20, 121, 165, 20);
+		contentPane.add(textFieldLink);
+		textFieldLink.setColumns(10);
+		
+		
+		// Creating a new ComboBoxModel and adding three elements to it.
+		DefaultComboBoxModel<String> TypeList = new DefaultComboBoxModel<String>();
+		TypeList.addElement("Imagen");
+		TypeList.addElement("Audio");
+		TypeList.addElement("Video");
+		
+		JComboBox<String> ComboBoxMediType = new JComboBox<String>();
+		ComboBoxMediType.setBounds(20, 54, 91, 22);
+		ComboBoxMediType.setModel(TypeList);
+		ComboBoxMediType.setSelectedIndex(0);
+		contentPane.add(ComboBoxMediType);
+		
+		
+		
+		JLabel lblNewLabel = new JLabel("Elige el tipo de multimedia que deseas postear");
+		lblNewLabel.setBounds(20, 22, 261, 28);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblCopiaAquiEl = new JLabel("Copia aqui el link de donde obtuviste la imagen");
+		lblCopiaAquiEl.setBounds(21, 94, 274, 28);
+		contentPane.add(lblCopiaAquiEl);
+		
+		JButton btnChooseFile = new JButton("Selecciona Archivo");
+		btnChooseFile.setBounds(20, 187, 165, 22);
+		contentPane.add(btnChooseFile);
+		
+		JLabel lblEligeElArchivo = new JLabel("Elige el archivo de tu computadora, para crear tu post");
+		lblEligeElArchivo.setBounds(20, 159, 295, 28);
+		contentPane.add(lblEligeElArchivo);
+		
+		
+		btnChooseFile.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Hola");
+				final JFileChooser  fileDialog = new JFileChooser();
+				
+				
+				int returnVal = fileDialog.showOpenDialog(null);
+				if (returnVal == JFileChooser.APPROVE_OPTION) {
+					
+					try {
+						String filePath = fileDialog.getSelectedFile().getAbsolutePath();
+						System.out.println(filePath);
+						String LinkOfPost = textFieldLink.getText();
+						
+						//Depending on the type of media that is selected, a diferente object is created
+						if(ComboBoxMediType.getSelectedIndex() == 0) {
+							ImagePost newVidPost = AppManage.CreateImagePostFromMetaData(null, LinkOfPost, filePath);
+						}
+						
+						else if(ComboBoxMediType.getSelectedIndex() == 1) {
+							ImagePost newVidPost = AppManage.CreateImagePostFromMetaData(null, LinkOfPost, filePath);
+						}
+						
+						else if(ComboBoxMediType.getSelectedIndex() == 2) {
+							ImagePost newVidPost = AppManage.CreateImagePostFromMetaData(null, LinkOfPost, filePath);
+						}
+						
+					} catch (Exception e2) {
+						// TODO: handle exception
+					}
+				}
+				
+			}});
 	}
-
 }
