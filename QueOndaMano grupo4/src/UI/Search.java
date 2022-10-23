14e4b48f325c30f2e598b4cc29a7e1a0c5c58a5f
FabiDeG <fabiola.d.g.12@gmail.com>
2022-10-23 16:54:50 -0600

    Search again

--- QueOndaMano grupo4/src/UI/Search.java
+++ QueOndaMano grupo4/src/UI/Search.java
@@ -9,13 +9,16 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import controller.AppManagement;
 import controller.FileManager;
+import model.Audio;
 import model.EmojiPost;
 import model.ImagePost;
 import model.MediaPost;
 import model.Post;
 import model.TxtPost;
 import model.User;
+import model.Video;
 
 import javax.swing.JTextField;
 import javax.swing.JButton;
@@ -29,14 +32,16 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import javax.swing.SwingConstants;
+import javax.swing.JLabel;
 
 public class Search extends JFrame {
-
+	
 	private JPanel contentPane;
 	private JTextField txtSearch;
 	private JTextField textField_1;
 	JList lstPost;
-
+	private JLabel lblParaLasFechas;
+	
 	/**
 	 * Launch the application.
 	 */
@@ -52,14 +57,13 @@ public class Search extends JFrame {
 			}
 		});
 	}
-
 	/**
 	 * Create the frame.
 	 */
 	public Search(ArrayList<User> theUsers, String search) {
 		
 		FileManager manager = new FileManager();
-		
+		AppManagement AppManage = new AppManagement();
 		
 		
 		ArrayList<User> Alusers = manager.getUsersFromFile();
@@ -94,46 +98,61 @@ public class Search extends JFrame {
 		contentPane.add(textField_1);
 		
 		lstPost = new JList();
-		lstPost.setBounds(31, 113, 513, 114);
+		lstPost.setBounds(20, 136, 646, 365);
 		contentPane.add(lstPost);
 		
 		
 		// Creating a new ComboBoxModel and adding three elements to it corresponding to the type of medio posts.
 			DefaultComboBoxModel<String> TypeList = new DefaultComboBoxModel<String>();
-			TypeList.addElement("Imagen");
-			TypeList.addElement("Audio");
-			TypeList.addElement("Video");				
+			TypeList.addElement("Usuario");
+			TypeList.addElement("Hashtag");
+			TypeList.addElement("Fecha");				
 			JComboBox<String> ComboBoxMediType = new JComboBox<String>();
-			ComboBoxMediType.setBounds(20, 80, 91, 22);
+			ComboBoxMediType.setBounds(241, 80, 91, 22);
 			ComboBoxMediType.setModel(TypeList);
 			ComboBoxMediType.setSelectedIndex(0);
 			contentPane.add(ComboBoxMediType);
+			
+			JLabel lblNewLabel = new JLabel("Que tipo de busqueda desea realizar?");
+			lblNewLabel.setBounds(20, 71, 226, 31);
+			contentPane.add(lblNewLabel);
+			
+			lblParaLasFechas = new JLabel("Para las fechas se debe buscar con el formato (DD/MM/AAAA) como 01/08/2022 por ejemplo, y los hastagas con el #");
+			lblParaLasFechas.setBounds(20, 106, 665, 31);
+			contentPane.add(lblParaLasFechas);
 		
 		btnSearch.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				//---------------------------------------------------
 				
+				
 				DefaultListModel<String> PostList = new DefaultListModel<String>();
-				
-				
-				
 				System.out.println(Alusers);
-				
-				
-				
-				
-				
-				
+				ArrayList<Post> PostsToShow = new ArrayList<Post>(); 
+				//Search only if the space is no blank
 				if(!txtSearch.getText().equals("")){
-					for (User user : Alusers) {
-						
-						System.out.println(user.getUserPosts());
-						for (Post aPost : user.getUserPosts()) {
+					//Search by Username
+					if(ComboBoxMediType.getSelectedIndex() == 0) {
+						PostsToShow = AppManage.SearchPostByAuthor(txtSearch.getText(), Alusers);
+					}
+					
+					//Search by Hastag
+					else if(ComboBoxMediType.getSelectedIndex() == 1) {
+						PostsToShow = AppManage.SearchPostByhastag(txtSearch.getText(), Alusers);
+					}
+					
+					//Search by date
+					else if(ComboBoxMediType.getSelectedIndex() == 2) {
+						PostsToShow = AppManage.SearchPostByDate(txtSearch.getText(), Alusers);
+					}
+					
+					
+						for (Post aPost : PostsToShow) {
 							
 							
 							if (aPost.getPostType() == 1) {
 								System.out.println("Emoji");
-								PostList.addElement("Por: " + user.getUsername() + " " +  aPost.getDate() + "Post: "+ ((EmojiPost) aPost).getPostEmoji() + " Likes " + aPost.getLikes());
+								PostList.addElement("Por: " + aPost.getAuthor() + " " +  aPost.getDate() + "Post: "+ ((EmojiPost) aPost).getPostEmoji() + " Likes " + aPost.getLikes());
 								
 								String listString = String.join(", ", aPost.getHashtags());
 								PostList.addElement(listString);
@@ -143,7 +162,7 @@ public class Search extends JFrame {
 							
 							else if (aPost.getPostType() == 2) {
 								System.out.println("txt");
-								PostList.addElement("Por: " + user.getUsername() + " " +  aPost.getDate() + " Post: "+ ((TxtPost) aPost).getPostMSG() + " Likes " + aPost.getLikes());
+								PostList.addElement("Por: " + aPost.getAuthor() + " " +  aPost.getDate() + " Post: "+ ((TxtPost) aPost).getPostMSG() + " Likes " + aPost.getLikes());
 								
 								String listString = String.join(", ", aPost.getHashtags());
 								PostList.addElement(listString);
@@ -153,7 +172,10 @@ public class Search extends JFrame {
 							
 							else if (aPost.getPostType() == 3) {
 								System.out.println("Aud");
-								PostList.addElement("Emoji\nA");
+								
+								PostList.addElement("Autor:  " + aPost.getAuthor() + " " + aPost.getDate() + " Link: " + ((MediaPost) aPost).getLink()+ " SampleRatio " + ((Audio) aPost).getSampleRatio()
+										+ " BitDepth " + ((Audio) aPost).getBitDepth() + " Size In KB: " + ((Audio) aPost).getProgramSize() + " Likes: " + aPost.getLikes());
+	
 								
 								String listString = String.join(", ", aPost.getHashtags());
 								PostList.addElement(listString);
@@ -163,10 +185,9 @@ public class Search extends JFrame {
 							
 							else if (aPost.getPostType() == 4) {
 								System.out.println("Img");
-								PostList.addElement("Autor: " + aPost.getAuthor() + "" + aPost.getDate() + "Link: " + ((MediaPost) aPost).getLink()+ "Resolucion: " + ((ImagePost) aPost).getResolution()+ "Format: " + ((ImagePost) aPost).getFormat() + "Likes: " + aPost.getLikes());
-								
-								
-								
+								PostList.addElement("Autor:  " + aPost.getAuthor() + " " + aPost.getDate() + " Link: " + ((MediaPost) aPost).getLink()+ " Resolucion: " + ((ImagePost) aPost).getResolution()
+										+ " Format: " + ((ImagePost) aPost).getFormat() + " Size In KB: " + ((ImagePost) aPost).getProgramSize() + " Likes: " + aPost.getLikes());
+	
 								String listString = String.join(", ", aPost.getHashtags());
 								PostList.addElement(listString);
 								
@@ -176,7 +197,8 @@ public class Search extends JFrame {
 							
 							else if (aPost.getPostType() == 5) {
 								System.out.println("vid");
-								PostList.addElement("Emoji\nA");
+								PostList.addElement("Autor:  " + aPost.getAuthor() + " " + aPost.getDate() + " Link: " + ((MediaPost) aPost).getLink()+ " FrameRate " + ((Video) aPost).getFrameRate()
+										+ " Size In KB: " + ((Audio) aPost).getProgramSize() + " Likes: " + aPost.getLikes());
 								
 								String listString = String.join(", ", aPost.getHashtags());
 								PostList.addElement(listString);
@@ -188,11 +210,8 @@ public class Search extends JFrame {
 						}
 						
 						lstPost.setModel(PostList);
-					}
-					
 					
 				}
-
 				else {
 					JOptionPane.showMessageDialog(null, "Ingrese un criterio de busqueda", "Informacion", JOptionPane.WARNING_MESSAGE, null);
 				}
